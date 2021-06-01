1f7147b24b15d3908b83b14c7d3816e2fb2b4b5b
julia fachin <juliafso@gmail.com>
2021-06-01 16:29:53 -0300

    routes feita

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :showtimes, except: %i[edit update destroy] do
+    resources :tickets, only: %i[index new create]
+  end
+
 end
