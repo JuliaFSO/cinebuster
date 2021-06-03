d4f7917a7bb2d1eca291c746d8f02cdd62f1f126
Emerson Silva <emrsnwslv@gmail.com>
2021-06-03 15:23:38 -0300

    ajuste

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -1,5 +1,5 @@
 class ShowtimesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [ :index, :show]
+    skip_before_action :authenticate_user!, only: [ :index, :show ]
 
     def index
       @showtimes = Showtime.where(available: true)
